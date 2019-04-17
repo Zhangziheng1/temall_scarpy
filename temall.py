f6c2dcba3c3d4c1096dbe7545cf67bc73151ecdd
Ziheng_wind <43085710+Zhangziheng1@users.noreply.github.com>
2019-04-17 19:02:25 +0800

    Update temall.py

--- temall.py
+++ temall.py
@@ -18,7 +18,6 @@ import fangtang
 
 """
 3.fangtang为自己封装的一个api，用于推送服务器的消息给用户【这么说。。真要脸】
-5.对于天猫有好几个不同的网址，所以在main()中设置了简单的host替换，如果不换就404
 """
 
 # 将爬虫获取的数据写入本地，是价格对比，获得每次爬取和上次爬取的价格进行对比，不同的结果对应不同的消息推送了
@@ -26,7 +25,7 @@ import fangtang
 
 def flie_change(title, post_money, price, other):
     # 进入日志目录
-    os.chdir('/root/ziheng/logs/')  # 进入logs目录
+    os.chdir('__YOUR_FLIE__/logs/')  # 进入logs目录
     time_filename = str(time.strftime('%Y.%m.%d', time.localtime(time.time())))
     with open(time_filename+'.txt', 'a+') as f:
         f.write(str(price))
@@ -73,7 +72,7 @@ def url(url_new, header):
         return False
 
 def main():
-    os.chdir('/root/ziheng/')
+    os.chdir('/__YOUR_FLIE__/)
     # 读取本地url文件，获取temall的url地址
     with open('url.txt', 'r') as f:
         url_frist = f.readlines()
@@ -98,7 +97,7 @@ def main():
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
         'Connection': 'keep-alive',
-        'Cookie': 't=edc8b3e37d5a6aa1fa671c8e4f92d029; cna=0tA1FWrG71sCAbYgMTK0FSn3; isg=BLa23S1B9q6HfYLLstzcrFUtBOqy1P1-kzyejiCfUBk0Y1b9iGeQIRwSe_-qUPIp; l=bBTv7DYuvkAZ0N81BOfwCZ111C79XIR44kPPhhmGvICP9N5B574lWZs3ePY6C31Vw1EvR3SVNIDpBeYBcQC..; thw=cn; uc3=vt3=F8dByEiVgCD9oAfeyx4%3D&id2=VyyUwjXHwa5x6w%3D%3D&nk2=saDUZFBwM%2BQ%3D&lg2=W5iHLLyFOGW7aA%3D%3D; tracknick=%5Cu4E00%5Cu7897%5Cu80A0%5Cu7C89; lgc=%5Cu4E00%5Cu7897%5Cu80A0%5Cu7C89; _cc_=W5iHLLyFfA%3D%3D; tg=0; mt=ci=21_1; enc=LfDm6GXnvOzezdvS7i9q%2FTGl18jWc2sHqK2S7IRsDzcDtVEO7r2I4dWIx7j0d%2BFepKcWf37QmQLP6LrOG3otsA%3D%3D; hng=CN%7Czh-CN%7CCNY%7C156; cookie2=1be5441af9776e43e7130bde01f0229a; _tb_token_=efbee03155d83; v=0',
+        'Cookie': __YOUR_COOKIE__
         'Upgrade-Insecure-Requests': '1',
         'TE': 'Trailers'
         }
